e95175a2311d1ef353f75e0f5088cd08d4bd3ebe
Sean Ochoa <sean.m.ochoa@gmail.com>
2021-11-12 10:22:35 -0800

    Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Snacks',
+    version='1.0',
+    description='Small snacks of helpful python code',
+    author='sochoa',
+    packages=['snacks'])
